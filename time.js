de6498898e217eb42e14d8ab331dbb36dd595d7d
divyag17 <52523871+divyag17@users.noreply.github.com>
2019-07-14 15:13:13 +0530

    Update time.js

--- time.js
+++ time.js
@@ -9,107 +9,120 @@ const save = () =>{
     tname=document.getElementById('tname').value;
     tskill=document.getElementById('tskill').value;
     
-    localStorage.setItem(tname,JSON.stringify({tname:tname,tskill:tskill}));
+    localStorage.setItem("POP",JSON.stringify({tname:tname,tskill:tskill}));
        
         
-    };
+};
 const trainerdetails=()=>
 {   
     clas=document.getElementById('class').value;
     session=document.getElementById('session').value;
     sub=document.getElementById('sub').value;
     trainer=document.getElementById('trainer').value;
-    localStorage.setItem("classdetail",JSON.stringify({class:clas,session:session,subject:sub,trainer:trainer}));
+    localStorage.setItem(tname,JSON.stringify({class:clas,session:session,subject:sub,trainer:trainer}));
        
 };
 const search=()=>
 {const a=[];
-    const table=document.getElementById('row');
+    
     if(document.getElementById('search').value=="classA")
     {
-        if(search=='classA'){
+        var two= new timetable();
+        const key=two.classupdate(document.getElementById('search').value);
+        two.buildrow(key);
+   
+    }
+    else{
+        var two= new timetable();
+        const key=two.classupdate(document.getElementById('search').value);
+        two.buildrow(key);
+    }
+}
+class timetable{
+    classupdate(s)
+    {
+    const a=[];
+    if(s=='classA'){
+        Object.keys(localStorage).forEach((e)=>{
+            const emp = JSON.parse(localStorage.getItem(e));
+            if(emp.class=='class A')
+                a.push(emp);
+
+            
+    });}
+    if(s=='classB'){
+        Object.keys(localStorage).forEach((e)=>{
+            const emp = JSON.parse(localStorage.getItem(e));
+            if(emp.class=='class B')
+                a.push(emp);
+
+            
+    });}
+        return a;
+    }
+    update(sub)
+    {
+    const a=[];
+        if(sub=='maths'){
+        Object.keys(localStorage).forEach((e)=>{
+            const emp = JSON.parse(localStorage.getItem(e));
+            if(emp.tskill=='maths')
+            {
+                a.push(emp);
+
+            }
+
+        });}
+        if(sub=='science'){
+        Object.keys(localStorage).forEach((e)=>{
+            const emp = JSON.parse(localStorage.getItem(e));
+            if(emp.tskill=='science')
+            {
+                a.push(emp);
+
+            }
+
+        });}
+        if(sub=='social'){
+        Object.keys(localStorage).forEach((e)=>{
+            const emp = JSON.parse(localStorage.getItem(e));
+            if(emp.tskill=='social')
+            {
+                a.push(emp);
+
+            }
+
+        });}
+        if(sub=='english'){
             Object.keys(localStorage).forEach((e)=>{
                 const emp = JSON.parse(localStorage.getItem(e));
-                if(emp.class=='classA')
+                if(emp.tskill=='english')
                 {
-                    if(emp.session=='s1')
-                    {
-                        rows+=`<td></td> `
-                    }
+                    a.push(emp);
+        
                 }
         
             });
-    };
-    table.innerHTML=rows;
+        }
+    return a;
     }
-    else{
+    buildTable = (key) =>{
+        const table=document.getElementById('trainer');
+        let rows = '';
+        key.forEach((table)=>{
+            rows += `<option value=${table.tname}> ${table.tname} </option> `
+        });
+        table.innerHTML=rows;
+        
+    }
+    buildrow= (key) =>{
         const table=document.getElementById('row');
     let rows = '';
-   table.forEach((a)=>{
-        rows += `
-                <td></td> `
-    });
-    table.innerHTML=rows;
-    }
-
-    }
-
-
-class timetable{
-
-update(sub)
-{
-const a=[];
-if(sub=='maths'){
-    Object.keys(localStorage).forEach((e)=>{
-        const emp = JSON.parse(localStorage.getItem(e));
-        if(emp.tskill=='maths')
-        {
-            a.push(emp);
-
-        }
-
-    });}
-if(sub=='science'){
-    Object.keys(localStorage).forEach((e)=>{
-        const emp = JSON.parse(localStorage.getItem(e));
-        if(emp.tskill=='science')
-        {
-            a.push(emp);
-
-        }
-
-    });}
-if(sub=='social'){
-    Object.keys(localStorage).forEach((e)=>{
-        const emp = JSON.parse(localStorage.getItem(e));
-        if(emp.tskill=='social')
-        {
-            a.push(emp);
-
-        }
-
-    });}
-    if(sub=='english'){
-        Object.keys(localStorage).forEach((e)=>{
-            const emp = JSON.parse(localStorage.getItem(e));
-            if(emp.tskill=='english')
-            {
-                a.push(emp);
-    
-            }
-    
-        });
-                    }
-return a;
-}
-buildTable = (key) =>{
-    const table=document.getElementById('trainer');
-    let rows = '';
     key.forEach((table)=>{
-        rows += `<option value=${table.tname}> ${table.tname} </option> `
+        rows += `<td>${table.trainer},${table.tskill},${table.session}</td> `
     });
     table.innerHTML=rows;
-    
+    }
+        
 }
-}
+
